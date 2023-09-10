621c5dd33f88cc22ebd868df2dc26273911f0813
David Fagan <davida.fagan@gmail.com>
2023-09-10 15:54:48 -0700

    Refactor didn't work
    Reverting back to the future bb.

--- src/js/BuildingGameObject.js
+++ src/js/BuildingGameObject.js
@@ -46,26 +46,30 @@ export default class BuildingGameObject {
     return true;
   }
 
-  findKey() {
+  isBuildingIgnited(cellX, cellY) {
     const key = Object.keys(this.#positions).find(
       (key) =>
         this.#positions[key].position[0] === cellX &&
         this.#positions[key].position[1] === cellY
     );
-  }
-
-  isBuildingIgnited(cellX, cellY) {
-    findKey();
     return this.#positions[key].isIgnited;
   }
 
   extinguish(cellX, cellY) {
-    findKey();
+    const key = Object.keys(this.#positions).find(
+      (key) =>
+        this.#positions[key].position[0] === cellX &&
+        this.#positions[key].position[1] === cellY
+    );
     this.#positions[key].isIgnited = false;
   }
 
   ignite(cellX, cellY) {
-    findKey();
+    const key = Object.keys(this.#positions).find(
+      (key) =>
+        this.#positions[key].position[0] === cellX &&
+        this.#positions[key].position[1] === cellY
+    );
     this.#positions[key].isIgnited = true;
   }
 
